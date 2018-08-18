92900e03b1d78830aafc61849bab629d8dbdfd2f
Srikanth <peruri.srikanthmca@gmail.com>
2018-08-17 23:04:39 -0700

    Committing the spring

---
+++ com.speruri.spring/src/main/java/com/speruri/spring/practice/App.java
@@ -0,0 +1,13 @@
+package com.speruri.spring.practice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
